2aae5d9c2a7be885f7e9eb5c2e4d401b35790f64
dev4us <bronxofsoul@gmail.com>
2020-02-27 22:36:14 +0900

    # 10. connect API & Create Loading Component

---
+++ components/Loader.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+import { ActivityIndicator, View } from "react-native";
+import { TINT_COLOR, BG_COLOR } from "../constans/Colors";
+
+const Loader = () => (
+  <Container>
+    <ActivityIndicator color={TINT_COLOR}></ActivityIndicator>
+  </Container>
+);
+
+const Container = styled(View)`
+  flex: 1;
+  background-color: ${BG_COLOR};
+  justify-content: center;
+`;
+
+export default Loader;
--- screens/MovieScreen.js
+++ screens/MovieScreen.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, TouchableOpacity } from "react-native";
+import Loader from "../components/Loader";
 
 const MovieScreen = ({ navigation }) => {
-  return (
+  const [loaded, setLoaded] = useState(false);
+
+  return loaded === false ? (
+    <Loader />
+  ) : (
     <>
       <Text>Movie</Text>
       <TouchableOpacity onPress={() => navigation.navigate("Detail")}>
