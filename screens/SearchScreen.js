ec41ff8dcee23ad32924f6640c43391c05f58132
dev4us <bronxofsoul@gmail.com>
2020-03-07 18:32:46 +0900

    # 21. design search Navigation

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { TextInput } from "react-native";
-import { BG_COLOR } from "../constants/Colors";
+import { BG_COLOR, GREY_COLOR } from "../constants/Colors";
+import Layout from "../constants/Layout";
 import styled from "styled-components";
 
 const SearchScreen = () => {
@@ -9,14 +10,24 @@ const SearchScreen = () => {
   const [search, setSearch] = useState("");
   const [loaded, setLoaded] = useState(false);
 
+  const onSubmitEditing = () => {
+    if (search !== "") {
+      alert("searching..");
+    }
+  };
   return (
     <Container>
-      <Input
-        onChangeText={e => setSearch(e)}
-        value={search}
-        autoFocus
-        returnKeyType={"search"}
-      ></Input>
+      <InputContainer>
+        <Input
+          onChangeText={e => setSearch(e)}
+          value={search}
+          autoFocus
+          returnKeyType="search"
+          placeholder="Search movies and tv"
+          placeholderTextColor={GREY_COLOR}
+          onSubmitEditing={onSubmitEditing}
+        ></Input>
+      </InputContainer>
     </Container>
   );
 };
@@ -25,7 +36,17 @@ const Container = styled.View`
   flex: 1;
   background-color: ${BG_COLOR};
 `;
+const InputContainer = styled.View`
+  align-items: center;
+  margin-vertical: 20px;
+`;
 
-const Input = styled.TextInput``;
+const Input = styled.TextInput`
+  background-color: rgba(255, 255, 255, 1);
+  width: ${Layout.width / 1.6}px;
+  border-radius: 20px;
+  padding: 10px;
+  text-align: center;
+`;
 
 export default SearchScreen;
