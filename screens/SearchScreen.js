034343ce03a2690c7b186a743d118d0a2f86b726
dev4us <bronxofsoul@gmail.com>
2020-03-07 18:23:30 +0900

    # 20. dev search Component

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,8 +1,31 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState } from "react";
+import { TextInput } from "react-native";
+import { BG_COLOR } from "../constants/Colors";
+import styled from "styled-components";
 
 const SearchScreen = () => {
-  return <Text>Search</Text>;
+  const [tvResults, setTvResults] = useState({});
+  const [movieResults, setMovieResults] = useState({});
+  const [search, setSearch] = useState("");
+  const [loaded, setLoaded] = useState(false);
+
+  return (
+    <Container>
+      <Input
+        onChangeText={e => setSearch(e)}
+        value={search}
+        autoFocus
+        returnKeyType={"search"}
+      ></Input>
+    </Container>
+  );
 };
+
+const Container = styled.View`
+  flex: 1;
+  background-color: ${BG_COLOR};
+`;
+
+const Input = styled.TextInput``;
 
 export default SearchScreen;
